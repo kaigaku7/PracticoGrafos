5d817bc4470f7908ea3b4fce1a21d2657913f970
kaigaku7 <82891434+kaigaku7@users.noreply.github.com>
2022-07-10 16:52:56 -0400

    Update RecorridoUtilsCostos.java

--- src/main/java/bo/edu/uagrm/inf310sb/ficct/grafos/pesados/recorrido_utils/RecorridoUtilsCostos.java
+++ src/main/java/bo/edu/uagrm/inf310sb/ficct/grafos/pesados/recorrido_utils/RecorridoUtilsCostos.java
@@ -32,38 +32,6 @@ public class RecorridoUtilsCostos {
         }
     }
 
-    /**
-     * 3)
-     * Retorna verdadero para indicar que un vertice está marcado.
-     * Falso en caso contrario.
-     * PRE: Se asume que la posición de vertice es una posicion válida.
-     * @param posDeVertice
-     * @return
-     */
-//    public boolean estaVerticeMarcado(int posDeVertice) {
-//        return this.marcados.get(posDeVertice);
-//    }
-
-    /**
-     * 4)
-     *
-     * @return
-     */
-//    public boolean estanTodosMarcados() {
-//        for (Boolean marcado: this.marcados) {
-//            if (!marcado) {
-//                return false;
-//            }
-//        }
-//
-//        return true;
-//    }
-
-    /**
-     * 5)
-     * PRE-Condicion: la posición de vértice, es una posicion válida.
-     * @param posDeVertice
-     */
     public void setCosto(int posDeVertice, double costo) {
         costos.set(posDeVertice, costo);
     }
